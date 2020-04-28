53b752a3ace2ecba4aa2478d5d32dfc489a14c93
IronSide25 <karol.pietrzak.25@gmail.com>
2020-04-28 19:15:33 +0200

    Init

---
+++ MobileSpace/Assets/Scripts/Starship.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Starship : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Input.GetAxis("Vertical"), 0, -Input.GetAxis("Horizontal"));
+        //transform.Rotate(-Input.acceleration.z, 0, -Input.acceleration.x);
+        transform.position += transform.forward;
+    }
+}
