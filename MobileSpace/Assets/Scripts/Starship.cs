70da646430392627af9dc6b9242bae8c2d15b158
IronSide25 <karol.pietrzak.25@gmail.com>
2020-05-09 20:57:27 +0200

    destruction system, accelerometer calibration

--- MobileSpace/Assets/Scripts/Starship.cs
+++ MobileSpace/Assets/Scripts/Starship.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class Starship : MonoBehaviour
 {
+    public float health = 100;
+    public Mesh mesh;
+
+    public GameObject explosionPrefab;
+    public int explosionRangeMin;
+    public int explosionRangeMax;
+    public GameObject firePrefab;
+    public int fireRangeMin;
+    public int fireRangeMax;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,8 +23,66 @@ public class Starship : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(Input.GetAxis("Vertical"), 0, -Input.GetAxis("Horizontal"));
-        //transform.Rotate(-Input.acceleration.z, 0, -Input.acceleration.x);
-        transform.position += transform.forward;
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.gameObject.name);
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        if(health > 0)
+        {
+            health -= 25;
+            Debug.Log(other.name);
+            if (health <= 0)
+                OnStarshipDestroy();
+        }       
+    }
+
+    private void OnStarshipDestroy()
+    {
+        //int explosionCount = Random.Range(explosionRangeMin, explosionRangeMax);
+        int fireCount = Random.Range(fireRangeMin, fireRangeMax);
+        StartCoroutine(ExplosionCoroutine(explosionPrefab, 5));
+        /*
+        for (int i = 0; i < explosionCount; i++)
+        {
+            Vector3 vertex = mesh.vertices[Random.Range(0, mesh.vertices.Length)];
+            GameObject go = Instantiate(explosionPrefab, transform.TransformPoint(vertex), Random.rotation);
+            float randomScale = Random.Range(2f, 4f);
+            go.transform.localScale = new Vector3(randomScale, randomScale, randomScale);
+        }*/
+        for (int j = 0; j < fireCount; j++)
+        {
+            
+            int rand = Random.Range(0, mesh.vertices.Length);
+            Vector3 vertex = mesh.vertices[rand];
+            GameObject go = Instantiate(firePrefab, transform.TransformPoint(vertex), Quaternion.identity);
+            ParticleSystem ps = go.GetComponent<ParticleSystem>();
+            /*
+            ParticleSystem.VelocityOverLifetimeModule velocityModule = ps.velocityOverLifetime;
+            velocityModule.x = 5;
+            go.transform.LookAt(transform.TransformPoint(vertex) + mesh.vertices[rand]);
+            Debug.DrawRay(transform.TransformPoint(vertex), mesh.vertices[rand]);
+            Debug.Break();*/
+        }
+    }
+
+    public IEnumerator ExplosionCoroutine(GameObject prefab, int count)
+    {
+        int explosionCount = Random.Range(explosionRangeMin, explosionRangeMax);
+        int fireCount = Random.Range(fireRangeMin, fireRangeMax);
+
+        for (int i = 0; i < explosionCount; i++)
+        {
+            Vector3 vertex = mesh.vertices[Random.Range(0, mesh.vertices.Length)];
+            GameObject go = Instantiate(explosionPrefab, transform.TransformPoint(vertex), Random.rotation);
+            float randomScale = Random.Range(2f, 4f);
+            go.transform.localScale = new Vector3(randomScale, randomScale, randomScale);
+            yield return new WaitForSeconds(Random.Range(0.1f, 0.6f));
+        }
     }
 }
---
+++ MobileSpace/Assets/Scripts/StarshipPlayer.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StarshipPlayer : MonoBehaviour
+{
+    Rigidbody rb;
+    public float rotationSpeed = 50;
+    Vector3 calibrationVec;
+
+    public Text t1;
+    public Text t2;
+    public Text t3;
+    public Text t4;
+    public Text t5;
+    public Text t6;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+        rb = GetComponent<Rigidbody>();
+        CalibrateButtonClicked();
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {      
+        Vector3 rotVector = Vector3.zero;
+        rotVector.x = Mathf.Clamp(-(Input.acceleration.y < 0 ? Input.acceleration.z : ((Mathf.Sign(Input.acceleration.z) * 2) - Input.acceleration.z)) + calibrationVec.z, -0.5f, 0.5f);
+        rotVector.z = Mathf.Clamp(-Input.acceleration.x + calibrationVec.x, -0.5f, 0.5f);
+
+        transform.Rotate(rotVector * rotationSpeed * Time.deltaTime);
+        transform.Rotate(Input.GetAxis("Vertical") * Time.deltaTime * 60, 0, -Input.GetAxis("Horizontal") * Time.deltaTime * 60);
+        transform.position += transform.forward * Time.deltaTime * 60;
+        //rb.AddTorque(Input.GetAxis("Vertical") * Time.deltaTime * 60, 0, -Input.GetAxis("Horizontal") * Time.deltaTime * 60, ForceMode.Acceleration);
+        //rb.velocity = transform.forward * Time.deltaTime * 60; ;
+
+
+        t1.text = "Acc z: " + Input.acceleration.z.ToString("F2");
+        t2.text = "Acc x: " + Input.acceleration.x.ToString("F2");
+        t3.text = "Acc y: " + Input.acceleration.y.ToString("F2");
+    
+        t4.text = "Rot x: " + (-(Input.acceleration.y < 0 ? Input.acceleration.z : ((Mathf.Sign(Input.acceleration.z) * 2) - Input.acceleration.z))).ToString("F2");
+        t5.text = "rotVector.x: " + rotVector.x.ToString("F2");
+        t6.text = "rotVector.z: " + rotVector.z.ToString("F2");
+    }
+
+    public void CalibrateButtonClicked()
+    {
+        calibrationVec = new Vector3(Input.acceleration.x, Input.acceleration.y, (Input.acceleration.y < 0 ? Input.acceleration.z : ((Mathf.Sign(Input.acceleration.z) * 2) - Input.acceleration.z)));
+    }
+}
